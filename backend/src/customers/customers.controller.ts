6ebf2b1ece8688bb860210308dd66d04eb1d4040
AllanLancioni <aclancioni@gmail.com>
2021-09-21 11:58:02 -0300

    ok

--- backend/src/customers/customers.controller.ts
+++ backend/src/customers/customers.controller.ts
@@ -5,7 +5,7 @@ export default class UsersController {
     try {
       res.status(200).json(await Service.list(req.query));
     } catch (error) {
-      res.status(400).send(error);
+      res.status(500).json({ status: 500, error });
     }
   }
 }
--- backend/src/customers/customers.service.ts
+++ backend/src/customers/customers.service.ts
@@ -2,34 +2,67 @@ import { Customer } from "./customers.model";
 
 export default class CustomerService {
   static async list(queryParams: any) {
-    console.log('list')
     try {
-      const resp = await Customer
-        .aggregate([
-          {
-            $lookup: {
-              from: 'orders',
-              let: { customerId: '$customer' },
-              pipeline: [
-                {
-                  $lookup: {
-                    from: 'bonds',
-                    foreignField: '_id',
-                    localField: 'bond',
-                    as: 'bond'
+      const data = await Customer.aggregate([
+        {
+          $lookup: {
+            from: 'orders',
+            let: { customerId: '$_id' },
+            pipeline: [
+              {
+                $match: {
+                  $expr: {
+                    $and: [
+                      { $eq: ['$customer', '$$customerId'] },
+                      ...(queryParams.defaulting ? [ { $eq: ['$paidAt', null] } ] : [])
+                    ]
                   }
-                },
-                {
-                  $unwind: '$bond'
                 }
-              ],
-              as: 'orders'
-            }
+              },
+              {
+                $sort: { boughtAt: 1 }
+              },
+              {
+                $lookup: {
+                  from: 'bonds',
+                  foreignField: '_id',
+                  localField: 'bond',
+                  as: 'bond'
+                }
+              },
+              { $unwind: '$bond' }
+            ],
+            as: 'orders'
+          },
+        },
+        {
+          $unwind: {
+            path: '$orders',
+            preserveNullAndEmptyArrays: false
           }
-        ])
-      console.log(resp)
-      return resp;
+        },
+        {
+          $group: {
+            _id: '$_id',
+            name: { $first: '$name' },
+            email: { $first: '$email' },
+            document: { $first: '$document' },
+            orders: { $push: '$orders' }
+          }
+        }
+      ]);
+
+      data.forEach(customer => {
+        const orderFound = customer.orders.find(order => !order.paidAt);
+        customer.isDefaulting = !!orderFound;
+        customer.since = orderFound ? orderFound.boughtAt : null;
+        delete customer.orders
+
+      });
+
+      return { data, count: data.length };
     } catch (error) {
+      console.log(error);
       throw error;
     }
   }
